d5013ad4760ee0fb6d2c770faa20bfe8f651c2a5
consensuscon <csmith@consensusnetworks.com>
2020-02-13 18:44:30 -0500

    made mods to node_exporter

--- harden.py
+++ harden.py
@@ -14,16 +14,16 @@ def install_deps():
 
 def configure_node_exporter():
     os.chdir("/home/sysadmin/")
-    make_node_user = subprocess.Popen('sudo useradd --no-create-home --shell /bin/false node_exporter', shell=True, stdin=None, executable="/bin/bash")
+    make_node_user = subprocess.Popen('useradd --no-create-home --shell /bin/false node_exporter', shell=True, stdin=None, executable="/bin/bash")
     make_node_user.wait()
     get_node_exporter = subprocess.Popen('wget https://github.com/prometheus/node_exporter/releases/download/v0.18.1/node_exporter-0.18.1.linux-amd64.tar.gz && tar xvf node_exporter-0.18.1.linux-amd64.tar.gz && cd node_exporter-0.18.1.linux-amd64',
                                           shell=True, stdin=None, executable="/bin/bash")
     get_node_exporter.wait()
-    set_permissions = subprocess.Popen('sudo cp node_exporter-0.18.1.linux-amd64.tar.gz/node_exporter /usr/local/bin && sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter',
+    set_permissions = subprocess.Popen('cp node_exporter /usr/local/bin && chown node_exporter:node_exporter /usr/local/bin/node_exporter',
                                         shell=True, stdin=None, executable="/bin/bash")
     set_permissions.wait()
     os.chdir("/home/sysadmin/")
-    clean_up = subprocess.Popen('sudo rm -rf node_exporter-0.18.1.linux-amd64', shell=True, stdin=None, executable="/bin/bash")
+    clean_up = subprocess.Popen('rm -rf node_exporter-0.18.1.linux-amd64 && node_exporter-0.18.1.linux-amd64.tar.gz', shell=True, stdin=None, executable="/bin/bash")
     clean_up.wait()
     os.chdir("/etc/systemd/system/")
     f = open("node_exporter.service", "w+")
