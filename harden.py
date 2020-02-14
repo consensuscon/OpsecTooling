60da4e4d621609c39374051ddda55bf021e2a1d2
consensuscon <csmith@consensusnetworks.com>
2020-02-13 20:13:14 -0500

    fix

--- harden.py
+++ harden.py
@@ -6,7 +6,7 @@ def install_deps():
     packages = ["fail2ban", "clamp", "clamav-daemon", "debsums", "aide", "libpam-cracklib",
                 "acct", "sysstat", "auditd", "gcc", "libpcre3-dev", "zliblg-dev", "libluajit-5.1-dev",
                 "libpcap-dev", "openssl", "libssl-dev", "libnghttp2-dev", "libdumbnet-dev", "bison", 
-                "flex", "libnetd"]
+                "flex", "libnetd", " zlib1g-dev "]
 
     for package in packages:
         process = subprocess.Popen('sudo apt-get install -y ' + package, shell=True, stdin=None, executable="/bin/bash")
@@ -76,7 +76,7 @@ def configure_snort():
                 "cp ~/snort/snort-2.9.15.1/etc/*.conf* /etc/snort", "cp  ~/snort/snort-2.9.15.1/etc/*.map /etc/snort"]
 
     for command in commands:
-        process = subprocess.Popen(command, hell=True, stdin=None, executable="/bin/bash")
+        process = subprocess.Popen(command, shell=True, stdin=None, executable="/bin/bash")
         process.wait()
     print('config done!')
     # snort_config('ldconfig && ln -s /usr/loca/bin/snort /usr/sbin/snort && grouped snort')
